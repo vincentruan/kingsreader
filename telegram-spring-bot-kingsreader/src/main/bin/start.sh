73d9e78e5b1eaf2f1a9aa33f409035ed2a776dd9
vincentruan <rzw0813@gmail.com>
2018-04-15 23:27:35 +0800

    first commit

--- telegram-spring-bot-kingsreader/src/main/bin/start.sh
+++ telegram-spring-bot-kingsreader/src/main/bin/start.sh
@@ -1,5 +1,44 @@
-#!/bin/bash
-DIR=`pwd`
+#!/bin/sh
+
+#check JAVA_HOME & java
+noJavaHome=false
+if [ -z "$JAVA_HOME" ] ; then
+    noJavaHome=true
+fi
+if [ ! -e "$JAVA_HOME/bin/java" ] ; then
+    noJavaHome=true
+fi
+if $noJavaHome ; then
+    echo
+    echo "Error: JAVA_HOME environment variable is not set."
+    echo
+    exit 1
+fi
+#==============================================================================
+
+#set JAVA_OPTS
+JAVA_OPTS="-server -Xms1024m -Xmx1024m -Xmn256m -Xss256k"
+#performance Options
+JAVA_OPTS="$JAVA_OPTS -XX:+AggressiveOpts"
+JAVA_OPTS="$JAVA_OPTS -XX:+UseBiasedLocking"
+JAVA_OPTS="$JAVA_OPTS -XX:+UseFastAccessorMethods"
+JAVA_OPTS="$JAVA_OPTS -XX:+DisableExplicitGC"
+JAVA_OPTS="$JAVA_OPTS -XX:+UseParNewGC"
+JAVA_OPTS="$JAVA_OPTS -XX:+UseConcMarkSweepGC"
+JAVA_OPTS="$JAVA_OPTS -XX:+CMSParallelRemarkEnabled"
+JAVA_OPTS="$JAVA_OPTS -XX:+UseCMSCompactAtFullCollection"
+JAVA_OPTS="$JAVA_OPTS -XX:+UseCMSInitiatingOccupancyOnly"
+JAVA_OPTS="$JAVA_OPTS -XX:CMSInitiatingOccupancyFraction=75"
+#GC Log Options
+#JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCApplicationStoppedTime"
+#JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCTimeStamps"
+#JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDetails"
+#debug Options
+#JAVA_OPTS="$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=8065,server=y,suspend=n"
+#==============================================================================
+
+#set HOME
+CURR_DIR=`pwd`
 cd `dirname $0`
 cd ../lib
 LIB_DIR=`pwd`
@@ -14,7 +53,7 @@ fi
 
 LIB_JARS=`ls ${LIB_DIR} | grep .jar | awk '{print "'${LIB_DIR}'/"$0}' | tr "\n" ":"`
 cd ..
-nohup java -classpath ${LIB_JARS} ${SERVER_NAME} nohup.server.out 2>&1 &
+nohup java -classpath ${LIB_JARS} ${JAVA_OPTS} ${SERVER_NAME} nohup.server.out 2>&1 &
 
 echo "start "${SERVER_NAME}" success!"
-cd ${DIR}
+cd ${CURR_DIR}
--- telegram-spring-bot-kingsreader/src/main/bin/stop.sh
+++ telegram-spring-bot-kingsreader/src/main/bin/stop.sh
@@ -1,4 +1,27 @@
-#!/bin/bash
+#!/bin/sh
+#set JAVA_HOME
+#JAVA_HOME=/usr/alibaba/java
+
+#check JAVA_HOME & java
+noJavaHome=false
+if [ -z "$JAVA_HOME" ] ; then
+    noJavaHome=true
+fi
+if [ ! -e "$JAVA_HOME/bin/java" ] ; then
+    noJavaHome=true
+fi
+if $noJavaHome ; then
+    echo
+    echo "Error: JAVA_HOME environment variable is not set."
+    echo
+    exit 1
+fi
+#==============================================================================
+
+#set JAVA_OPTS
+JAVA_OPTS="-Xss256k"
+#==============================================================================
+
 DIR=`pwd`
 cd `dirname $0`
 cd ../lib
