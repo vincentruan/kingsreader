6d180a753182eca86610fe269ff4c73e7fd170eb
vincentruan <rzw0813@gmail.com>
2018-04-22 22:29:57 +0800

    first commit

---
+++ dropbox-spring-boot-starter/src/main/java/com/github/springtg/dropbox/config/DropboxConfig.java
@@ -0,0 +1,40 @@
+package com.github.springtg.dropbox.config;
+
+import com.dropbox.core.DbxAppInfo;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.DbxWebAuth;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for dropbox settings.
+ * @author vincentruan
+ */
+@Configuration
+@ConditionalOnClass({DbxRequestConfig.class})
+@EnableConfigurationProperties(DropboxConfigProperties.class)
+public class DropboxConfig {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    private final DropboxConfigProperties dropboxConfigProperties;
+
+    public DropboxConfig(final DropboxConfigProperties dropboxConfigProperties) {
+        this.dropboxConfigProperties = dropboxConfigProperties;
+    }
+
+    @Bean
+    public DbxRequestConfig dbxRequestConfig() {
+        return new DbxRequestConfig(appName);
+    }
+
+    @Bean
+    public DbxWebAuth dbxWebAuth(final DbxRequestConfig requestConfig) {
+        final DbxAppInfo appInfo = new DbxAppInfo(dropboxConfigProperties.getKey(), dropboxConfigProperties.getSecret());
+        return new DbxWebAuth(requestConfig, appInfo);
+    }
+}
--- telegram-spring-boot-starter/src/main/java/com/github/springtg/bot/platform/client/TelegramBotHttpClient.java
+++ telegram-spring-boot-starter/src/main/java/com/github/springtg/bot/platform/client/TelegramBotHttpClient.java
@@ -13,10 +13,29 @@ import java.util.concurrent.Future;
  * @since 22/05/2016
  */
 public interface TelegramBotHttpClient {
+
+    /**
+     * 异步调用http get
+     * @param method 后缀，对应baseurl/{}
+     * @param params get请求参数
+     * @param returnType 返回类型
+     * @param <T>
+     * @return
+     * @throws TelegramBotApiException
+     */
     <T> Future<T> executeGet(@NotNull String method,
                              @Nullable Map<String, String> params,
                              @NotNull JavaType returnType) throws TelegramBotApiException;
 
+    /**
+     * 异步调用http get
+     * @param method 后缀，对应baseurl/{}
+     * @param requestObject post请求参数
+     * @param returnType 返回类型
+     * @param <T>
+     * @return
+     * @throws TelegramBotApiException
+     */
     <T, V> Future<T> executePost(
             @NotNull String method,
             @Nullable V requestObject,
