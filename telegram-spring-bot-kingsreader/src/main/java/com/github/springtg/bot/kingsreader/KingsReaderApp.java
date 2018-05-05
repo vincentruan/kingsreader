281c8fa776347b726dc37642dd41a05d7eeee2fd
vincentruan <rzw0813@gmail.com>
2018-05-05 18:35:39 +0800

    first commit

--- telegram-spring-bot-kingsreader/src/main/java/com/github/springtg/bot/kingsreader/KingsReaderApp.java
+++ telegram-spring-bot-kingsreader/src/main/java/com/github/springtg/bot/kingsreader/KingsReaderApp.java
@@ -15,9 +15,14 @@
  */
 package com.github.springtg.bot.kingsreader;
 
+import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.logging.BotLogger;
+
+import java.util.logging.Level;
 
 /**
  * @author vincentruan
